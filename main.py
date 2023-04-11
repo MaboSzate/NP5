6cb3954573533a8d527ef37d8cbebcce50065ea0
Salamon András <andras.salamon@stud.uni-corvinus.hu>
2023-04-11 11:22:06 +0200

    1ora vege

--- main.py
+++ main.py
@@ -6,60 +6,60 @@ import seaborn as sns
 
 
 def check_etf_price():
-etf = 'VOO'
-df = u.read_etf_file(etf)
-df[['Close', 'Adj Close']].plot()
-plt.show()
-# check_etf_price()
+    etf = 'VOO'
+    df = u.read_etf_file(etf)
+    df[['Close', 'Adj Close']].plot()
+    plt.show()
+    # check_etf_price()
 
 
 def test_return_calculation():
-u.get_etf_returns('VOO')
-# test_return_calculation()
+    u.get_etf_returns('VOO')
+    # test_return_calculation()
 
 
 def test_plot_dividend_return():
-df = u.get_dividend_return('VOO', 'simple')
-df.plot()
-plt.show()
-# test_plot_dividend_return()
+    df = u.get_dividend_return('VOO', 'simple')
+    df.plot()
+    plt.show()
+    # test_plot_dividend_return()
 
 
 def test_plot_total_return():
-df = u.get_total_return('VOO', 'log')
-df.plot()
-plt.show()
-# test_plot_total_return()
+    df = u.get_total_return('VOO', 'log')
+    df.plot()
+    plt.show()
+    # test_plot_total_return()
 
 
 def test_portfolio_return():
-d_pf = {'VOO': 0.6, 'IEI': 0.4}
-u.get_portfolio_return(d_pf)
-# test_portfolio_return()
+    d_pf = {'VOO': 0.6, 'IEI': 0.4}
+    u.get_portfolio_return(d_pf)
+    # test_portfolio_return()
 
 
 def demonstrate_list_comprehension():
-xs = [1, 2, 5]
-# using normal for
-result1 = []
-for x in xs:
-result1.append(x*2)
-print(result1)
-# using list comprehension
-result2 = [x * 2 for x in xs]
-print(result2)
-# demonstrate_list_comprehension()
+    xs = [1, 2, 5]
+    # using normal for
+    result1 = []
+    for x in xs:
+        result1.append(x*2)
+    print(result1)
+    # using list comprehension
+    result2 = [x * 2 for x in xs]
+    print(result2)
+    # demonstrate_list_comprehension()
 
 
 def demonstrate_historical_var():
-d_weights = {'IEI': 0.1, 'VOO': 0.9}
-df_pf = u.get_portfolio_return(d_weights)
-# calculate percentile by using calculated index
-df_sorted = df_pf.sort_values(by='pf')
-df_sorted.index = [x/len(df_sorted) for x in range(1, len(df_sorted)+1)]
-print(df_sorted.head(20))
-# calculate var by using quantile
-print(df_pf.quantile([0.1, 0.05, 0.025, 0.01]))
+    d_weights = {'IEI': 0.1, 'VOO': 0.9}
+    df_pf = u.get_portfolio_return(d_weights)
+    # calculate percentile by using calculated index
+    df_sorted = df_pf.sort_values(by='pf')
+    df_sorted.index = [x/len(df_sorted) for x in range(1, len(df_sorted)+1)]
+    print(df_sorted.head(20))
+    # calculate var by using quantile
+    print(df_pf.quantile([0.1, 0.05, 0.025, 0.01]))
 
 
 demonstrate_historical_var()
--- test_utils.py
+++ test_utils.py
@@ -10,67 +10,96 @@ get_portfolio_return, calc_historical_var, osszead
 
 class Sajat(unittest.TestCase):
 
-def test_calc_historical_var(self):
-d_weights = {'IEI': 0.6, 'VOO': 0.4}
-pf_value = 100000
-l_conf_levels = [0.95, 0.99]
-last_day_of_interval = '2020-03-01'
-window_in_days = 250
-df_var, df_var_amount =\
-u.calc_historical_var(pf_value, d_weights, l_conf_levels, last_day_of_interval, window_in_days)
-self.assertAlmostEqual(df_var.values[0][0], -0.00429, 5)
-self.assertAlmostEqual(df_var.values[1][0], -0.01039, 5)
+    def test_calc_historical_var(self):
+        d_weights = {'IEI': 0.6, 'VOO': 0.4}
+        pf_value = 100000
+        l_conf_levels = [0.95, 0.99]
+        last_day_of_interval = '2020-03-01'
+        window_in_days = 250
+        df_var, df_var_amount =\
+        u.calc_historical_var(pf_value, d_weights, l_conf_levels, last_day_of_interval, window_in_days)
+        self.assertAlmostEqual(df_var.values[0][0], -0.00429, 5)
+        self.assertAlmostEqual(df_var.values[1][0], -0.01039, 5)
 
-def test_get_portfolio_return_btw_dates(self):
-d_weights = {'IEI': 0.6, 'VOO': 0.4}
-df = u.get_portfolio_return_btw_dates(d_weights, '2020-03-01', '2020-04-15')
-self.assertEqual(len(df.index), 32)
-self.assertAlmostEqual(32.1111, 32.1112, 3)
+    def test_calc_simple_var(self):
+        d_weights = {'IEI': 0.6, 'VOO': 0.4}
+        pf_value = 100000
+        l_conf_levels = [0.95, 0.99]
+        last_day_of_interval = '2020-03-01'
+        window_in_days = 250
+        df_var, df_var_amount =\
+        u.calc_simple_var(pf_value, d_weights, l_conf_levels, last_day_of_interval, window_in_days)
+        self.assertAlmostEqual(df_var.values[0][0], -0.00459, 4)
+        self.assertAlmostEqual(df_var.values[0][1], -0.00663, 4)
+        pass
 
-def test_sutract_trading_date(self):
-result = u.subtract_trading_date('2023-03-28', 2)
-self.assertEqual(result, '2023-03-24')
+    def test_calc_covar_var(self):
+        d_weights = {'IEI': 0.6, 'VOO': 0.4}
+        pf_value = 100000
+        l_conf_levels = [0.95, 0.99]
+        last_day_of_interval = '2020-03-01'
+        window_in_days = 250
+        df_var, df_var_amount =\
+        u.calc_covar_var(pf_value, d_weights, l_conf_levels, last_day_of_interval, window_in_days)
+        self.assertAlmostEqual(df_var.values[0][0], -0.00459, 4)
+        self.assertAlmostEqual(df_var.values[0][1], -0.00663, 4)
+
+
+
+
+
+    def test_get_portfolio_return_btw_dates(self):
+        d_weights = {'IEI': 0.6, 'VOO': 0.4}
+        df = u.get_portfolio_return_btw_dates(d_weights, '2020-03-01', '2020-04-15')
+        self.assertEqual(len(df.index), 32)
+        self.assertAlmostEqual(32.1111, 32.1112, 3)
+
+    def test_sutract_trading_date(self):
+        result = u.subtract_trading_date('2023-03-28', 2)
+        self.assertEqual(result, '2023-03-24')
 
 
 class MyTest(unittest.TestCase):
 
-def test_osszead(self):
-c = osszead(2, 3)
-self.assertEqual(c, 5)
+    def test_osszead(self):
+        c = osszead(2, 3)
+        self.assertEqual(c, 5)
 
-def test_read_etf_file(self):
-df = read_etf_file('IEI')
-self.assertIsInstance(df, pd.DataFrame)
+    def test_read_etf_file(self):
+        df = read_etf_file('IEI')
+        self.assertIsInstance(df, pd.DataFrame)
 
-def test_get_etf_returns(self):
-df = get_etf_returns('IEI')
-self.assertIsInstance(df, pd.DataFrame)
-self.assertEqual(df.columns[0], 'IEI')
+    def test_get_etf_returns(self):
+        df = get_etf_returns('IEI')
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(df.columns[0], 'IEI')
 
-def test_get_total_return(self):
-df = get_total_return('IEI')
-self.assertIsInstance(df, pd.DataFrame)
-self.assertEqual(df.columns[0], 'IEI')
+    def test_get_total_return(self):
+        df = get_total_return('IEI')
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(df.columns[0], 'IEI')
 
-def test_get_dividend_return(self):
-df = get_dividend_return('IEI')
-self.assertIsInstance(df, pd.DataFrame)
-self.assertEqual(df.columns[0], 'IEI')
+    def test_get_dividend_return(self):
+        df = get_dividend_return('IEI')
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(df.columns[0], 'IEI')
 
-def test_get_price_return(self):
-df = get_price_return('IEI')
-self.assertIsInstance(df, pd.DataFrame)
-self.assertEqual(df.columns[0], 'IEI')
+    def test_get_price_return(self):
+        df = get_price_return('IEI')
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(df.columns[0], 'IEI')
 
-def test_get_portfolio_return(self):
-d_weights = {'SPY': 0.5, 'QQQ': 0.5}
-df = get_portfolio_return(d_weights)
-self.assertIsInstance(df, pd.DataFrame)
-self.assertEqual(df.columns[0], 'pf')
+    def test_get_portfolio_return(self):
+        d_weights = {'SPY': 0.5, 'QQQ': 0.5}
+        df = get_portfolio_return(d_weights)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(df.columns[0], 'pf')
 
-def test_calc_historical_var(self):
-d_weights = {'SPY': 0.5, 'QQQ': 0.5}
-l_conf_levels = [0.95, 0.99]
-df = calc_historical_var(d_weights, l_conf_levels)
-self.assertIsInstance(df, pd.Series)
-self.assertEqual(df.index.tolist(), l_conf_levels)
+    def test_calc_historical_var(self):
+        d_weights = {'SPY': 0.5, 'QQQ': 0.5}
+        l_conf_levels = [0.95, 0.99]
+        df = calc_historical_var(d_weights, l_conf_levels)
+        self.assertIsInstance(df, pd.Series)
+        self.assertEqual(df.index.tolist(), l_conf_levels)
+
+
